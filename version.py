a8e8604979053ad7593567d5a00b40954bcdac68
Sensei <johndoe@example.com>
2026-01-13 20:54:02 +0300

    v0.0.23 - bug fix

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Версия Starvell Cardinal
 """
 
-VERSION = "0.0.22"
+VERSION = "0.0.23"
 GITHUB_REPO = "Hackep1551/Starvell-cardinal"
 VERSION_URL = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main/version.py"
 
